189be9b0921205f48a959a9732fadb758a468882
GeorgiyEloev <geloev.exceedteam@gmail.com>
2022-01-14 15:03:59 +0300

    complet login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./components/Login/Login"
+import Login from "./components/Login/Login";
 import "./App.scss";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      {/* <Route path="">
-      </Route>*/}
+      <Route path="/registration" element={<p>registration</p>} />
+      <Route path="/main" element={<p>main</p>} />
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,99 @@
-import React from "react";
-import { AppBar, Box, TextField, Container, Button } from "@mui/material";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import {
+  AppBar,
+  Box,
+  TextField,
+  Container,
+  Button,
+  Snackbar,
+} from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
 import logo from "../../img/logo.png";
 import Vector from "../../img/Vector.png";
 import "./Login.scss";
 
 const Login = () => {
+  const [dataLogin, dataLoginEdit] = useState({
+    login: "",
+    password: "",
+  });
+
+  const [open, setOpen] = useState(false);
+  const [snackbar, setSnackbar] = useState({
+    message: "",
+    status: "",
+  });
+
+  const navigation = useNavigate();
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+  const state = {
+    vertical: "top",
+    horizontal: "center",
+  };
+
+  const { message, status } = snackbar;
+  const { vertical, horizontal } = state;
+  const { login, password } = dataLogin;
+
+  const passwordRegular = password.match(/^(?=.*\d)[a-z\d]{6,}$/gi);
+  const loginRegular = login.match(/^[a-z\d]{6,}$/gi);
+
+  const loginSystem = async () => {
+    if (passwordRegular && loginRegular) {
+      await axios
+        .post("http://localhost:8000/user/authorizationUser", {
+          login: login.trim(),
+          password: password.trim(),
+        })
+        .catch((err) => {
+          setSnackbar({
+            message: "Неверный логин или пароль!!!",
+            status: "error",
+          });
+          dataLoginEdit({
+            login: "",
+            password: "",
+          });
+          handleClick();
+        })
+        .then((results) => {
+          navigation("/main");
+          localStorage.setItem("token", results.data.data.token);
+        });
+    } else {
+      setSnackbar({
+        message: `Длина логина не меньше 6 символов. Все символы латинского алфавита или
+				цифры!!!
+				Длина пароля не меньше 6 символов. Все символы латинского алфавита.
+				Пароль должна содержать обязательно хотя бы одно число.!!!`,
+        status: "warning",
+      });
+      dataLoginEdit({
+        login: "",
+        password: "",
+      });
+      handleClick();
+    }
+  };
+
   return (
     <div className="login-main">
       <AppBar className="label-header">
@@ -14,22 +103,57 @@ const Login = () => {
       <Container className="container-style">
         <img src={Vector} alt="Vector" className="img-vector" />
         <Box className="box-style">
-          <h1>Войти в систему</h1>
-          <p>Login:</p>
-          <TextField id="outlined-required" label="Login" />
-          <p>Password:</p>
-          <TextField
-            id="outlined-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-          />
+          <div className="group-login">
+            <h1>Войти в систему</h1>
+            <p>Login:</p>
+            <TextField
+              id="outlined-required"
+              label="Login"
+              value={login}
+              onChange={(event) =>
+                dataLoginEdit({ login: event.target.value, password: password })
+              }
+            />
+            <p>Password:</p>
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(event) =>
+                dataLoginEdit({ login: login, password: event.target.value })
+              }
+            />
+          </div>
           <div className="group-button">
-            <Button variant="text">Text</Button>
-            <Button variant="outlined">Outlined</Button>
+            <Button
+              className="button-style"
+              variant="outlined"
+              onClick={() => loginSystem()}
+            >
+              Войти
+            </Button>
+            <Button
+              className="button-style"
+              variant="text"
+              onClick={() => navigation("/registration")}
+            >
+              Зарегистрироваться
+            </Button>
           </div>
         </Box>
       </Container>
+      <Snackbar
+        anchorOrigin={{ vertical, horizontal }}
+        open={open}
+        autoHideDuration={10000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity={status} sx={{ width: "100%" }}>
+          {message}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
