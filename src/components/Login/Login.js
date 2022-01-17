37c56ababc196caa07a11b29446926acd81e0f00
GeorgiyEloev <geloev.exceedteam@gmail.com>
2022-01-17 10:13:38 +0300

    complet Registration

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,7 +12,7 @@ import {
 import MuiAlert from "@mui/material/Alert";
 import logo from "../../img/logo.png";
 import Vector from "../../img/Vector.png";
-import "./Login.scss";
+import "../LoginRegist.scss";
 
 const Login = () => {
   const [dataLogin, dataLoginEdit] = useState({
@@ -37,14 +37,7 @@ const Login = () => {
   };
 
   const Alert = React.forwardRef((props, ref) => {
-    return (
-      <MuiAlert
-        elevation={6}
-        ref={ref}
-        variant="filled"
-        {...props}
-      />
-    );
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
   });
 
   const state = {
@@ -56,57 +49,27 @@ const Login = () => {
   const { vertical, horizontal } = state;
   const { login, password } = dataLogin;
 
-  const passwordRegular = password.match(/^(?=.*\d)[a-z\d]{6,}$/gi);
-  const loginRegular = login.match(/^[a-z\d]{6,}$/gi);
-
   const loginSystem = async () => {
-    if (loginRegular) {
-      if (passwordRegular) {
-        await axios
-          .post("http://localhost:8000/user/authorizationUser", {
-            login: login.trim(),
-            password: password.trim(),
-          })
-          .then((results) => {
-            navigation("/main");
-            localStorage.setItem("token", results.data.data.token);
-          })
-          .catch((err) => {
-            setSnackbar({
-              message: "Неверный логин или пароль!!!",
-              status: "error",
-            });
-            dataLoginEdit({
-              login: "",
-              password: "",
-            });
-            handleClick();
-          });
-      } else {
+    await axios
+      .post("http://localhost:8000/user/authorizationUser", {
+        login: login.trim(),
+        password: password.trim(),
+      })
+      .then((results) => {
+        navigation("/main");
+        localStorage.setItem("token", results.data.data.token);
+      })
+      .catch((err) => {
         setSnackbar({
-          message: `Длина пароля не меньше 6 символов. 
-						Все символы латинского алфавита.
-						Пароль должен содержать обязательно хотя бы одно число.!!!`,
-          status: "warning",
+          message: "Неверный логин или пароль!!!",
+          status: "error",
         });
         dataLoginEdit({
-          login: login,
+          login: "",
           password: "",
         });
         handleClick();
-      }
-    } else {
-      setSnackbar({
-        message: `Длина логина не меньше 6 символов. 
-					Все символы латинского алфавита или цифры!!!`,
-        status: "warning",
       });
-      dataLoginEdit({
-        login: "",
-        password: password,
-      });
-      handleClick();
-    }
   };
 
   return (
@@ -116,11 +79,7 @@ const Login = () => {
         <h1>Войти в систему</h1>
       </AppBar>
       <Container className="container-style">
-        <img 
-					src={Vector} 
-					alt="Vector" 
-					className="img-vector"
-				/>
+        <img src={Vector} alt="Vector" className="img-vector" />
         <Box className="box-style">
           <div className="group-login">
             <h1>Войти в систему</h1>
@@ -174,11 +133,7 @@ const Login = () => {
         autoHideDuration={10000}
         onClose={handleClose}
       >
-        <Alert
-          onClose={handleClose}
-          everity={status}
-          className="alert-style"
-        >
+        <Alert onClose={handleClose} severity={status} className="alert-style">
           {message}
         </Alert>
       </Snackbar>
